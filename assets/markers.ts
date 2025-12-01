e6aa13e1efecc4f88a120635d422646ff4003b60
Nathan Hayes <hayesn98@gmail.com>
2025-12-01 14:30:30 -0600

    3

---
+++ assets/markers.ts
@@ -0,0 +1,45 @@
+export const markers = [
+	// San Francisco
+	{
+		latitude: 37.7749,
+		longitude: -122.4194,
+		latitudeDelta: 0.01,
+		longitudeDelta: 0.01,
+		name: 'San Francisco City Center'
+	},
+	{
+		latitude: 37.8077,
+		longitude: -122.475,
+		latitudeDelta: 0.01,
+		longitudeDelta: 0.01,
+		name: 'Golden Gate Bridge'
+	},
+	{
+		latitude: 35.20606,
+		longitude: -97.44232,
+		latitudeDelta: 0.01,
+		longitudeDelta: 0.01,
+		name: 'Oklahoma Memorial Stadium'
+	},
+	{
+		latitude: 35.46980,
+		longitude: -97.52050,
+		latitudeDelta: 0.01,
+		longitudeDelta: 0.01,
+		name: 'Oklahoma City Museum of Art'
+	},
+	{
+		latitude: 35.26025,
+		longitude: -97.77314,
+		latitudeDelta: 0.01,
+		longitudeDelta: 0.01,
+		name: 'Tiger Safari Zoological Park'
+	},
+	{
+		latitude: 36.15352,
+		longitude: -95.94628,
+		latitudeDelta: 0.01,
+		longitudeDelta: 0.01,
+		name: 'The University of Tulsa'
+	}
+];
